077e5edcb00ee41df6ddedaea26e62ddf881be1e
mitstaku <takuya.mitsuhori@gmail.com>
2024-01-05 11:56:52 +0900

    金額が登録できる

--- app/controllers/event_versions_controller.rb
+++ app/controllers/event_versions_controller.rb
@@ -7,10 +7,18 @@ class EventVersionsController < ApplicationController
   def create
     @event_form = EventForm.new(event_form_params)
     @event = Event.find(params[:event_id])
+
+    # 現在の日付をinput_dateに設定
+    @event_form.input_date = Date.today
+
+    # 既存のイベントバージョンの数をカウントし、1を加えてversion_numberに設定
+    @event_form.version_number = EventVersion.where(event_id: @event.id).count + 1
+
     if @event_form.valid?
       @event_form.save
       redirect_to root_path, notice: 'イベントバージョンが正常に作成されました。'
     else
+      Rails.logger.error @event_form.errors.full_messages
       render :new
     end
   end
@@ -18,7 +26,16 @@ class EventVersionsController < ApplicationController
   private
 
   def event_form_params
-    params.require(:event_form).permit(:version_number, :details, :cost, :input_date, :ceremony_id, :detail)
+    params.require(:event_form).permit(
+      :adult_count, :child_count, :ceremony_category_id, :ceremony_cost, :ceremony_detail,
+      :food_category_id, :food_cost, :food_detail, :venue_category_id, :venue_cost, :venue_detail,
+      :costume_category_id, :costume_cost, :costume_detail,
+      :beauty_category_id, :beauty_cost, :beauty_detail, :flower_category_id, :flower_cost, :flower_detail,
+      :print_category_id, :print_cost, :print_detail, :staging_category_id, :staging_cost,
+      :staging_detail, :photo_category_id, :photo_cost, :photo_detail, :video_category_id,
+      :video_cost, :video_detail, :gift_category_id, :gift_cost, :gift_detail,
+      :pre_ceremony_id, :pre_ceremony_cost, :pre_ceremony_detail
+    )
           .merge(event_id: params[:event_id])
   end
 end
--- app/models/event_form.rb
+++ app/models/event_form.rb
@@ -1,75 +1,142 @@
 class EventForm
   include ActiveModel::Model
-  attr_accessor :event_id, :version_number, :details, :cost, :input_date,
+  attr_accessor :event_id, :version_number, :details, :input_date, :ceremony_category_id, :ceremony_detail, :ceremony_cost,
                 :adult_count, :child_count,
-                :ceremony_id, :detail, :event_version_id,
-                :event_version, :food_id, :venue_id, :print_id,
-                :staging_id, :photo_id, :video_id, :gift_id,
-                :pre_ceremony_id, :beauty_category_id, :costume_category_id,
-                :flower_category_id, :food_category_id, :gift_category_id,
-                :photo_category_id, :print_category_id, :staging_category_id,
-                :venue_category_id, :video_category_id, :beauty_cost,
-                :costume_cost, :flower_cost,
-                :food_cost,
-                :gift_cost,
-                :photo_cost,
-                :pre_ceremony_cost,
-                :print_cost,
-                :staging_cost,
-                :venue_cost,
-                :video_cost
+                :food_category_id, :food_detail, :food_cost, :venue_category_id, :venue_detail, :venue_cost,
+                :costume_category_id, :costume_detail, :costume_cost,
+                :beauty_category_id, :beauty_detail, :beauty_cost, :flower_category_id, :flower_detail, :flower_cost,
+                :print_category_id, :print_detail, :print_cost, :staging_category_id, :staging_detail, :staging_cost,
+                :photo_category_id, :photo_detail, :photo_cost, :video_category_id, :video_detail, :video_cost,
+                :gift_category_id, :gift_detail, :gift_cost,
+                :pre_ceremony_id, :pre_ceremony_detail, :pre_ceremony_cost, :cost
 
-  validates :ceremony_id, :detail, :cost, :event_id, :version_number,
-            :details, :input_date, :beauty_category_id, :costume_category_id,
-            :flower_category_id, :food_category_id, :gift_category_id,
-            :photo_category_id, :pre_ceremony_id, :print_category_id,
-            :staging_category_id, :venue_category_id, :video_category_id,
-            :adult_count, :child_count, presence: true
+  validates :version_number, :input_date, :ceremony_category_id, :ceremony_detail, :ceremony_cost,
+            :adult_count, :child_count,
+            :food_category_id, :food_detail, :food_cost, :venue_category_id, :venue_detail, :venue_cost,
+            :costume_category_id, :costume_detail, :costume_cost,
+            :beauty_category_id, :beauty_detail, :beauty_cost, :flower_category_id, :flower_detail, :flower_cost,
+            :print_category_id, :print_detail, :print_cost, :staging_category_id, :staging_detail, :staging_cost,
+            :photo_category_id, :photo_detail, :photo_cost, :video_category_id, :video_detail, :video_cost,
+            :gift_category_id, :gift_detail, :gift_cost,
+            :pre_ceremony_id, :pre_ceremony_detail, :pre_ceremony_cost, presence: true
 
   def save
     return false unless valid?
 
     calculate_total_cost # 合計金額を計算
     ActiveRecord::Base.transaction do
+      # 現在の日付をinput_dateに設定
+      self.input_date = Date.today
+
+      # 既存のイベントバージョンの数をカウントし、1を加えてversion_numberに設定
+      self.version_number = EventVersion.where(event_id:).count + 1
       event_version = EventVersion.create!(
-        ceremony_id:,
         event_id:,
         version_number:,
-        details:,
+        details: 'Some details',
         cost:,
-        input_date:,
-        beauty_category_id:,
-        costume_category_id:,
-        flower_category_id:,
-        food_category_id:,
-        gift_category_id:,
-        photo_category_id:,
-        pre_ceremony_id:,
-        print_category_id:,
-        staging_category_id:,
-        venue_category_id:,
-        video_category_id:,
+        input_date:
+      )
+      guest = Guest.create!(
+        event_version_id: event_version.id,
         adult_count:,
         child_count:
       )
+      ceremony = Ceremony.create!(
+        event_version_id: event_version.id,
+        ceremony_category_id:,
+        detail: ceremony_detail,
+        cost: ceremony_cost
+      )
+      food = Food.create!(
+        event_version_id: event_version.id,
+        food_category_id:,
+        detail: food_detail,
+        cost: food_cost
+      )
+      venue = Venue.create!(
+        event_version_id: event_version.id,
+        venue_category_id:,
+        detail: venue_detail,
+        cost: venue_cost
+      )
+      costume = Costume.create!(
+        event_version_id: event_version.id,
+        costume_category_id:,
+        detail: costume_detail,
+        cost: costume_cost
+      )
+      beauty = Beauty.create!(
+        event_version_id: event_version.id,
+        beauty_category_id:,
+        detail: beauty_detail,
+        cost: beauty_cost
+      )
+      flower = Flower.create!(
+        event_version_id: event_version.id,
+        flower_category_id:,
+        detail: flower_detail,
+        cost: flower_cost
+      )
+      print = Print.create!(
+        event_version_id: event_version.id,
+        print_category_id:,
+        detail: print_detail,
+        cost: print_cost
+      )
+      staging = Staging.create!(
+        event_version_id: event_version.id,
+        staging_category_id:,
+        detail: staging_detail,
+        cost: staging_cost
+      )
+      photo = Photo.create!(
+        event_version_id: event_version.id,
+        photo_category_id:,
+        detail: photo_detail,
+        cost: photo_cost
+      )
+      video = Video.create!(
+        event_version_id: event_version.id,
+        video_category_id:,
+        detail: video_detail,
+        cost: video_cost
+      )
+      gift = Gift.create!(
+        event_version_id: event_version.id,
+        gift_category_id:,
+        detail: gift_detail,
+        cost: gift_cost
+      )
+      pre_ceremony = PreCeremony.create!(
+        event_version_id: event_version.id,
+        pre_ceremony_id:,
+        detail: pre_ceremony_detail,
+        cost: pre_ceremony_cost
+      )
     end
-
-    def calculate_total_cost
-      self.cost = [
-        beauty_cost.to_i,
-        costume_cost.to_i,
-        flower_cost.to_i,
-        food_cost.to_i,
-        gift_cost.to_i,
-        photo_cost.to_i,
-        pre_ceremony_cost.to_i,
-        print_cost.to_i,
-        staging_cost.to_i,
-        venue_cost.to_i,
-        video_cost.to_i
-      ].sum
-    end
-  rescue ActiveRecord::RecordInvalid
+    true
+  rescue ActiveRecord::RecordInvalid => e
+    Rails.logger.error e.record.errors
     false
+  end
+
+  private
+
+  def calculate_total_cost
+    self.cost = [
+      ceremony_cost.to_i,
+      food_cost.to_i,
+      venue_cost.to_i,
+      costume_cost.to_i,
+      beauty_cost.to_i,
+      flower_cost.to_i,
+      print_cost.to_i,
+      staging_cost.to_i,
+      photo_cost.to_i,
+      video_cost.to_i,
+      gift_cost.to_i,
+      pre_ceremony_cost.to_i
+    ].sum
   end
 end
